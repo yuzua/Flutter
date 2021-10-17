ed38006b70f32003e7abe4dd0892ce9de3d9f780
yuzua <k.saito70329@gmail.com>
2021-10-17 12:03:59 +0900

    [update] Widget Testの使用

--- flutter_sample_app/lib/main.dart
+++ flutter_sample_app/lib/main.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( new MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final title = 'Flutterサンプル';
-  final message = 'サンプル・メッセージ';
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      home: new MyHomePage(
-        title: this.title,
-        message: this.message
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
-  final String message;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  MyHomePage({this.title, this.message}): super();
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -35,23 +41,25 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
+  final String title;
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
+  int _counter = 0;
 
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,8 +76,8 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       // body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      //   // Center is a layout widget. It takes a single child and positions it
+      //   // in the middle of the parent.
       //   child: Column(
       //     // Column is also a layout widget. It takes a list of children and
       //     // arranges them vertically. By default, it sizes itself to fit its
@@ -97,10 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
       //     ],
       //   ),
       // ),
-      body: Text(
-        widget.message,
-        style: TextStyle(fontSize:32.0),
-      ),
+      body: Text("初めてのテキスト"),
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _incrementCounter,
       //   tooltip: 'Increment',
