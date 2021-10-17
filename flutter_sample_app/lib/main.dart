a12f7b3604e6885f1914f407310bf46663f96197
yuzua <k.saito70329@gmail.com>
2021-10-17 15:09:53 +0900

    [update] Widget Iconの使用

--- flutter_sample_app/lib/main.dart
+++ flutter_sample_app/lib/main.dart
@@ -112,6 +112,26 @@ class _MyHomePageState extends State<MyHomePage> {
           onPressed: () => {print("ボタンが押されたよ")},
           child: Text("更新"),
         ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              Icons.favorite,
+              color: Colors.pink,
+              size: 24.0,
+            ),
+            Icon(
+              Icons.audiotrack,
+              color: Colors.green,
+              size: 30.0
+            ),
+            Icon(
+              Icons.beach_access,
+              color: Colors.blue,
+              size: 36.0,
+            )
+          ],
+        )
       ])
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _incrementCounter,
