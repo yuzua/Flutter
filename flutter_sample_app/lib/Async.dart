886c7eb153d7099e01f29889c3d87140bd55b471
yuzua <k.saito70329@gmail.com>
2021-11-20 15:53:16 +0900

    [update] thenを使った非同期処理の実装

--- flutter_sample_app/lib/Async.dart
+++ flutter_sample_app/lib/Async.dart
@@ -6,11 +6,20 @@ class Async {
     print("method begin");
     print(DateTime.now().toString());
     print("data1 start");
-    print(asyncFunc("data1", 3));
+    Future<String> result1 = asyncFunc("data1", 3);
+    result1.then((result) {
+      print(result);
+    });
     print("data2 start");
-    print(asyncFunc("data2", 2));
+    Future<String> result2 = asyncFunc("data2", 2);
+    result2.then((result) {
+      print(result);
+    });
     print("data3 start");
-    print(asyncFunc("data3", 1));
+    Future<String> result3 = asyncFunc("data3", 1);
+    result3.then((result) {
+      print(result);
+    });
   }
   // timeの時間分スリープし、その後現在時間を返す関数
   // Future<String> は将来的にStringが返却されるという意味
